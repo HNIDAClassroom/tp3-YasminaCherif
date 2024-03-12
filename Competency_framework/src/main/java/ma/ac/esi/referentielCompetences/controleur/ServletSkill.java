9c81dad442e8848be2e00486001db0ab5b78a03c
YasminaCherif <162153130+YasminaCherif@users.noreply.github.com>
2024-03-12 12:09:20 +0100

    Add files via upload
    Tp3 Yasmina Cherif

---
+++ Competency_framework/src/main/java/ma/ac/esi/referentielCompetences/controleur/ServletSkill.java
@@ -0,0 +1,58 @@
+package ma.ac.esi.referentielCompetences.controleur;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ma.ac.esi.referentielCompetences.model.Skill;
+import ma.ac.esi.referentielCompetences.model.SkillDAO;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class ServletSkill extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ServletSkill() {
+        super();
+       
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String description = request.getParameter("description");
+		String domain = request.getParameter("domain");
+		String level = request.getParameter("level");
+
+		Skill skill = new Skill(name, description, domain, level);
+		SkillDAO skillDAO = new SkillDAO();
+		boolean succes =  skillDAO.addSkill(skill); 
+		/*if (succes) { 
+		request.setAttribute("message", "La compétence a été ajoutée avec succès."); 
+		}  
+		else { 
+		request.setAttribute("erreur", "Erreur lors de l'ajout de la compétence."); 
+		} 
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/SkillCrud.jsp"); 
+		dispatcher.forward(request, response); 
+		request dispatcher transfere la requette  au fichier .jsp
+		request desipatcher permet d afficher la reponse de la requette a l utilisateur 
+		 */
+		//modification pour afficher une liste et non pas juste un message:
+		if (succes) {
+	        List<Skill> skills = skillDAO.getAllSkills();
+	        request.setAttribute("skills", skills);
+	        request.setAttribute("message", "Liste de compétences mise à jour avec succès.");
+	    } else {
+	        request.setAttribute("erreur", "Erreur lors de l'ajout de la compétence.");
+	    }
+        // Rediriger vers la page SkillCrud.jsp
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/SkillCrud.jsp");
+        dispatcher.forward(request, response);
+    }
+	}
+
